5f3c16aea515ee64dae06279830b9b4bfbd3ea9c
Franklin-Gomez <franklin_043097@hotmail.com>
2024-10-09 10:31:23 -0500

    working on show category

--- app/components/order/OrderSidebar.tsx
+++ app/components/order/OrderSidebar.tsx
@@ -1,4 +1,5 @@
 import { prisma } from "@/src/lib/prisma"
+import CategoryIcon from "../ui/CategoryIcon"
 
 async function getCategories() { 
     return await prisma.category.findMany()
@@ -8,11 +9,18 @@ export default async function OrderSidebar() {
 
     const categories = await getCategories()
 
-    console.log( categories )
-
     return (
         <div className="md:w-72 md:h-screen bg-white">
-            OrderSidebar
+
+            <nav className="mt-10">
+                { categories.map( category => ( 
+                    <CategoryIcon 
+                        key={category.id}
+                        category={category}
+                    />
+                ))}
+            </nav>
+
         </div>
     )
 }
