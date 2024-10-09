40364e4a53824928b630cd676324a2121fde695e
Franklin-Gomez <franklin_043097@hotmail.com>
2024-10-09 09:53:38 -0500

    fetch to get database info

--- app/components/order/OrderSidebar.tsx
+++ app/components/order/OrderSidebar.tsx
@@ -1,4 +1,15 @@
-export default function OrderSidebar() {
+import { prisma } from "@/src/lib/prisma"
+
+async function getCategories() { 
+    return await prisma.category.findMany()
+}
+
+export default async function OrderSidebar() {
+
+    const categories = await getCategories()
+
+    console.log( categories )
+
     return (
         <div className="md:w-72 md:h-screen bg-white">
             OrderSidebar
