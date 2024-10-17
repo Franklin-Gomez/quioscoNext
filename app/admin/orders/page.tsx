5a2786b53b71488c1444ca706189f0ae82d4139b
Franklin-Gomez <franklin_043097@hotmail.com>
2024-10-17 11:55:23 -0500

    get orders with products

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,6 +1,35 @@
 import Heading from "@/app/components/ui/Heading"
+import { prisma } from "@/src/lib/prisma"
 
-export default function page() {
+async function getPendingOrders(){
+
+  const orders = await prisma.order.findMany({
+
+    // nos traemos los que tengan status en false
+    where: { 
+      status: false
+    },
+
+    // nos traremos la informacion de la relacion con los productos
+    include :  { 
+      orderProduct : { 
+        include : { 
+          product : true
+        }
+      }
+    }
+
+  })
+
+  return orders
+
+}
+
+export default async function page() {
+
+  const orders = await getPendingOrders()
+  console.log( JSON.stringify( orders , null , 2))
+
   return (
     <>
       <Heading> Administrar Ordenes </Heading>
