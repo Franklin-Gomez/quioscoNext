ed8ca9584df6897becf3224bb481f48e4852c010
Franklin-Gomez <franklin_043097@hotmail.com>
2024-10-17 14:04:19 -0500

    show and types to order

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,3 +1,4 @@
+import OrderCard from "@/app/components/order/OrderCard"
 import Heading from "@/app/components/ui/Heading"
 import { prisma } from "@/src/lib/prisma"
 
@@ -28,11 +29,30 @@ async function getPendingOrders(){
 export default async function page() {
 
   const orders = await getPendingOrders()
-  console.log( JSON.stringify( orders , null , 2))
+  //console.log( JSON.stringify( orders , null , 2))
 
   return (
     <>
       <Heading> Administrar Ordenes </Heading>
+
+      { orders.length ?
+        
+        ( 
+          <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-5 mt-5">
+
+            { orders.map( order => ( 
+              <OrderCard
+                key={order.id}
+                order={order}
+              />
+            ))}
+
+          </div>
+        ) 
+          : 
+        <p className="text-center">No hay ordenes Pendientes</p> 
+      
+      }
     </>
   )
 }
