1e0908f7a193b7c9a848d0d6740fd971f601f0ca
Franklin-Gomez <franklin_043097@hotmail.com>
2024-10-18 14:56:14 -0500

    show all products

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,12 +1,29 @@
+import ProductTable from "@/app/components/products/ProductTablet";
 import Heading from "@/app/components/ui/Heading";
+import { prisma } from "@/src/lib/prisma";
 
-export default function page() {
+
+async function getProducts () { 
+
+  const products = await prisma.product.findMany()
+
+  return products
+
+}
+
+export default async function page() {
+
+  const products = await getProducts()
+
   return (
     <>
-      <Heading>
-        { 'Administrar Productos' }
-      </Heading>
+      <Heading> Administrar Productos </Heading>
+
+      <ProductTable
+        products={products}
+      />
     
     </>
   )
+
 }
