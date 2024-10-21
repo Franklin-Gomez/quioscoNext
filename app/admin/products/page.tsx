6e0b4ca94e1acc38898b7f4b27d23ff43bfff30a
Franklin-Gomez <franklin_043097@hotmail.com>
2024-10-21 09:26:43 -0500

    create pagination

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -3,9 +3,18 @@ import Heading from "@/app/components/ui/Heading";
 import { prisma } from "@/src/lib/prisma";
 
 
-async function getProducts () { 
+async function getProducts ( page : number , pageSize : number) { 
 
-  const products = await prisma.product.findMany({
+  const skip = ( page - 1 ) * pageSize // formularia para ir saltando paginacion
+
+  const products = await prisma.product.findMany({  
+
+    // trae solo 10 registos como un LIMIT en SQL
+    take: pageSize,
+
+    // salta dependiendo del monto
+    skip: skip,
+
     include : { 
       category : true
     }
@@ -19,9 +28,16 @@ async function getProducts () {
 // returnType eneric
 export type ProductWithCategory = Awaited<ReturnType<typeof getProducts>>
 
-export default async function page() {
+export default async function page( { searchParams } : { searchParams : { page : string }}) {
 
-  const products = await getProducts()
+  console.log( searchParams.page ) // recuperar los parametros pasados por la URL
+  
+  const page = +searchParams.page || 1 // si no tenemos un query string en la url es por que estamos en la pagina 1
+  
+  const pageSize = 10
+  
+  const products = await getProducts( page , pageSize)
+  
 
   return (
     <>
