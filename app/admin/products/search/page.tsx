a1dce6698b24381444cd23296401be30d41c3650
Franklin-Gomez <franklin_043097@hotmail.com>
2024-10-22 11:20:00 -0500

    show result

--- app/admin/products/search/page.tsx
+++ app/admin/products/search/page.tsx
@@ -1,12 +1,49 @@
+import ProductSearchForm from "@/app/components/products/ProductSearchForm";
+import ProductTable from "@/app/components/products/ProductTablet";
 import Heading from "@/app/components/ui/Heading";
+import { prisma } from "@/src/lib/prisma";
 
-export default function SearchPage( { searchParams } : { searchParams : { search : string}}) {
+async function searchProducts ( searchTerm : string ) { 
 
-    const search  = searchParams.search
+    const products = await prisma.product.findMany({
+        where: { 
+            name: { 
+                contains : searchTerm,
+                mode : 'insensitive', // ignorar las mayusculas
+            }
+        },
+        include: {
+            category: true
+        }
+    })
+
+    return products
+
+}
+
+export default async function SearchPage( { searchParams } : { searchParams : { search : string}}) {
+
+    const products = await searchProducts( searchParams.search )
 
     return (
         <>
-            <Heading> Pagina de Busqueda </Heading>
+
+            <Heading> Resultado de Busqueda : {searchParams.search} </Heading>
+
+            <div className="flex flex-col lg:flex-row lg:justify-end gap-5">
+
+                <ProductSearchForm/>
+
+            </div>
+
+            { products.length ? ( 
+                
+                <ProductTable
+                    products={products}
+                /> 
+
+            ) : <p className="text-center text-lg mt-5 font-bold" >No hay resultado</p>}
+
         
         </>
     )
