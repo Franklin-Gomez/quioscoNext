163a2eb7644a48c83ee2d2a967dc27e36e17ffe4
Franklin-Gomez <franklin_043097@hotmail.com>
2024-10-25 10:28:32 -0500

    revalidate data with swr automatic , admin/order

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,62 +1,38 @@
+'use Client'
+
 import OrderCard from "@/app/components/order/OrderCard"
 import Heading from "@/app/components/ui/Heading"
-import { prisma } from "@/src/lib/prisma"
-import { revalidatePath } from "next/cache"
+import { OrderWithProducts } from "@/src/types"
+import useSWR from "swr"
 
-async function getPendingOrders(){
 
-  const orders = await prisma.order.findMany({
 
-    // nos traemos los que tengan status en false
-    where: { 
-      status: false
-    },
+export default function page() {
 
-    // nos traremos la informacion de la relacion con los productos
-    include :  { 
-      orderProduct : { 
-        include : { 
-          product : true
-        }
-      }
-    }
+  
+  const url = '/admin/orders/api'
+  const fetcher = () => fetch( url ) // realiza la peticion
+    .then( res => res.json() )
+    .then( data => data )
+  
 
+  const { data , error , isLoading } = useSWR<OrderWithProducts[]>(url, fetcher, { 
+   refreshInterval: 60000,
+   revalidateOnFocus: false
   })
 
-  return orders
+  if( isLoading ) return 'Cargando...'
 
-}
-
-export default async function page() {
-
-  const orders = await getPendingOrders()
-  //console.log( JSON.stringify( orders , null , 2))
-
-  return (
+  if ( data ) return (
     <>
       <Heading> Administrar Ordenes </Heading>
 
-      <form
-        action={ async () => { 
-          "use server"
-          revalidatePath('/admin/orders')
-        }}
-      > 
-
-        <input
-          type="submit"
-          value='Actualizar Ordenes'
-          className='bg-amber-400 w-full lg:w-auto text-xl px-10 py-3 text-center font-bold cursor-pointer'
-        />
-
-      </form>
-
-      { orders.length ?
+      { data.length ?
         
         ( 
           <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-5 mt-5">
 
-            { orders.map( order => ( 
+            { data.map( order => ( 
               <OrderCard
                 key={order.id}
                 order={order}
