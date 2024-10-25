3a82cc94298ec133470d73dccaa5068d701d5c21
Franklin-Gomez <franklin_043097@hotmail.com>
2024-10-25 13:58:31 -0500

    show waitress page, to show the list of order ready

--- app/orders/page.tsx
+++ app/orders/page.tsx
@@ -1,11 +1,38 @@
+'use client'
+
+import { OrderWithProducts } from "@/src/types";
 import Logo from "../components/ui/Logo";
+import useSWR from "swr";
+import LatestOrderItem from "../components/ui/LatestOrderItem";
 
 export default function OrdersPage() { 
-    return (
+
+    const url = '/orders/api'
+    const fetcher = () => fetch(url).then( res => res.json() ).then( data => data )
+    
+    const { data , error , isLoading } = useSWR<OrderWithProducts[]>( url , fetcher, { 
+        refreshInterval: 60000,
+        revalidateOnFocus: false
+    })
+  
+    if( isLoading ) return 'Cargando...'
+
+    if ( data ) return (
         <>
             <h1 className="text-center mt-20 text-6xl font-black"> Ordenes Listas </h1>
 
             <Logo />
+
+            { data.length >= 1 ? ( 
+                <div className="grid grid-cols-1 gap-5 max-w-5xl mx-auto mt-10">
+                    { data.map( order => ( 
+                        <LatestOrderItem
+                            key={ order.id }
+                            order={ order }
+                        />
+                    ))}
+                </div>
+            ) : <p className="text-center my-10 "> No hay ordenes listas </p>}
         </>
     )
 }
